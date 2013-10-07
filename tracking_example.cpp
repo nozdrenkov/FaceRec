e1c24c67d52bb19cc8503cede9d3f55716d45301
Sergey Nozdrenkov <nozdrenkov@gmail.com>
2013-10-08 01:16:12 +0400

    calcOpticalFlowPyrLK

--- tracking_example.cpp
+++ tracking_example.cpp
@@ -12,6 +12,11 @@
 using namespace cv;
 using namespace std;
 
+float dist(const Point2f &a, const Point2f &b)
+{
+    return sqrt((a - b).dot(a - b));
+}
+
 void drawPt(Mat &img, const Point2f &pt, const Scalar &color)
 {
     Point2f dx(5, 0);
@@ -20,11 +25,20 @@ void drawPt(Mat &img, const Point2f &pt, const Scalar &color)
     line(img, pt - dy, pt + dy, color);
 }
 
+void drawVec(Mat &img, const Point2f &p1, const Point2f &p2, const Scalar &color)
+{
+    line(img, p1, p2, color);
+    line(img, p2, p2, color, 3);
+}
+
 int main()
 {
     const string path = "C:\\v1.mp4";
 
     VideoCapture cap(path);
+
+    Mat prev_frame;
+    vector<Point2f> prev_corners;
 
     Mat frame;
     while (cap.grab())
@@ -32,22 +46,48 @@ int main()
         cap.retrieve(frame);
         resize(frame, frame, Size(), 0.5, 0.5);
 
-        
-        vector<Point2f> corners;
+
         Mat cur_frame;
         cvtColor(frame, cur_frame, CV_BGR2GRAY);
-        goodFeaturesToTrack(cur_frame, corners, 500, 0.01, 0.1, noArray(), 3, false);
 
-        for (size_t i = 0; i < corners.size(); ++i)
-            drawPt(frame, corners[i], CV_RGB(0, 255, 0));
+        if (!prev_frame.empty())
+        {
+            if (prev_corners.empty())
+                goodFeaturesToTrack(prev_frame, prev_corners, 500, 0.01, 0.1, noArray(), 3, false);
+
+
+            vector<unsigned char> status;
+            vector<float> err;
+            vector<Point2f> cur_corners;
+            if (prev_corners.size())
+                calcOpticalFlowPyrLK(prev_frame, cur_frame, prev_corners, cur_corners, status, err);
+
+
+            for (size_t i = 0; i < cur_corners.size(); ++i)
+            {
+                if (status[i] && dist(prev_corners[i], cur_corners[i]) < 60)
+                    drawVec(frame, prev_corners[i], cur_corners[i], CV_RGB(0, 255, 0));
+                else
+                    drawPt(frame, cur_corners[i], CV_RGB(255, 0, 0));
+            }
+
+
+            prev_corners = cur_corners;
+            imshow("detector", frame);
+        }
+        prev_frame = cur_frame.clone();
 
 
 
-        imshow("detector", frame);
-
-        char c = waitKey(50);
+        char c = waitKey(100);
         if (c == 27)
             break;
+        if (c == ' ')
+            prev_corners.clear();
+
+
+        for (int i = 0; i < 2; ++i)
+            cap.grab();
     }
 }
 
