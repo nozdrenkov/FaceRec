1f0dd99ba16a5efcc6a2acf1490ae39aba71235a
Sergey Nozdrenkov <nozdrenkov@gmail.com>
2013-10-08 00:38:44 +0400

    Good features to track

--- tracking_example.cpp
+++ tracking_example.cpp
@@ -12,6 +12,14 @@
 using namespace cv;
 using namespace std;
 
+void drawPt(Mat &img, const Point2f &pt, const Scalar &color)
+{
+    Point2f dx(5, 0);
+    Point2f dy(0, 5);
+    line(img, pt - dx, pt + dx, color);
+    line(img, pt - dy, pt + dy, color);
+}
+
 int main()
 {
     const string path = "C:\\v1.mp4";
@@ -23,6 +31,17 @@ int main()
     {
         cap.retrieve(frame);
         resize(frame, frame, Size(), 0.5, 0.5);
+
+        
+        vector<Point2f> corners;
+        Mat cur_frame;
+        cvtColor(frame, cur_frame, CV_BGR2GRAY);
+        goodFeaturesToTrack(cur_frame, corners, 500, 0.01, 0.1, noArray(), 3, false);
+
+        for (size_t i = 0; i < corners.size(); ++i)
+            drawPt(frame, corners[i], CV_RGB(0, 255, 0));
+
+
 
         imshow("detector", frame);
 
